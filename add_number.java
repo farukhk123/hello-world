0a0cc90bf57108d6054eeb70d3b13671de164709
Farukh <68771092+farukhk123@users.noreply.github.com>
2022-04-14 12:05:34 +0530

    Add files via upload
    Addition of number

---
+++ add_number.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class add_number {
+    public static void main(String[] args) {
+        System.out.print(" the sum of these number is: ");
+        int num1 = 6;
+        int num2 = 6;
+        int num3 = 6;
+        int sum = num1+ num2 +num3;
+
+        System.out.println(sum);
+    }
+}
